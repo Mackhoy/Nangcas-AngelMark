048059c8b3af301d969dce55e5e53bf07016cec0
Mackhoy <occ.nangcas.angelmark.simbajon@gmail.com>
2025-02-15 11:04:52 +0800

    Updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id('userID');
+            $table->string('fullname');
+            $table->integer('contactNumber')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('dateofbirth');
+            $table->enum('userRole', ['admin', 'guest'])->default('guest');
+            $table->string('username')->unique();
+            $table->string('password')->unique();
             $table->timestamps();
         });
     }
